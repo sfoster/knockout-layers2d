deca5195ca7b29ac9a0243a66ddf800a4880954d
sfoster <sam@sam-i-am.com>
2012-09-18 13:56:54 +0100

    Ditch bogus optimization and just leave a TODO comment

--- js/knockout.layers2d.js
+++ js/knockout.layers2d.js
@@ -1,6 +1,6 @@
 define(['knockout'], function(ko){
   function getValue(value) {
-    if('function' === typeof value){
+    if(ko.isObservable(value)){
       value = ko.utils.unwrapObservable(value); 
     }
     return value;
@@ -8,15 +8,15 @@ define(['knockout'], function(ko){
   ko.bindingHandlers['layers2d'] = {
       'update': function (element, valueAccessor) {
         var layers = getValue(valueAccessor()); 
-        // FIXME: we don't want to call getContext every time, 
-        // but the viewModel value might be elsewhere, so storing it on there is not right
-        var ctx = layers._2dcontext || (layers._2dcontext = element.getContext('2d'));
+        // TODO: optimize - we don't want to call getContext every time, 
+        var ctx = element.getContext('2d');
         layers.forEach(function(layer){
           var instructions = getValue(layer); 
           instructions.forEach(function(instruct){
             if(instruct.assign) {
               ctx[instruct.assign] = instruct.value;
             } else if(instruct.call) {
+              // TODO: maybe require the value to always be an array to optimize a little here
               ctx[instruct.call].apply(ctx, instruct.value instanceof Array ? instruct.value : [instruct.value]);
             }
           })
