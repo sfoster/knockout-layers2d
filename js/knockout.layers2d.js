7dc552830a8667405ff40859d71b17f3d8622fa6
sfoster <sam@sam-i-am.com>
2012-09-18 12:03:50 +0100

    fixme comment on the getContext caching

--- js/knockout.layers2d.js
+++ js/knockout.layers2d.js
@@ -8,6 +8,8 @@ define(['knockout'], function(ko){
   ko.bindingHandlers['layers2d'] = {
       'update': function (element, valueAccessor) {
         var layers = getValue(valueAccessor()); 
+        // FIXME: we don't want to call getContext every time, 
+        // but the viewModel value might be elsewhere, so storing it on there is not right
         var ctx = layers._2dcontext || (layers._2dcontext = element.getContext('2d'));
         layers.forEach(function(layer){
           var instructions = getValue(layer); 
